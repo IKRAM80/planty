5f2144d4c7d4e423d839dd7a6ca60cf5733d6b23
Ikram Bchina <bchina.ikram@gmail.com>
2024-02-23 20:45:39 +0100

    page nous rencontrer et accueil  100% à finir le menu

--- wp-content/themes/blankslate_child/functions.php
+++ wp-content/themes/blankslate_child/functions.php
@@ -41,7 +41,32 @@ function theme_enqueue_styles() {
 		}
 		add_action('customize_register', 'your_theme_new_customizer_settings');
 		
-	
+/* HOOK FILTER */
+
+add_filter('show_admin_bar', '__return_false');
+
+add_filter('wp_nav_menu_items', 'admin_link', 10, 2);
+
+
+function admin_link($items, $args)
+{
+
+    if (is_user_logged_in()) {
+        $adminlink = admin_url();
+        $siteurl = site_url();
+        $items .= '<li class="menu-item"><a href="' . site_url('/nous-rencontrer') . '">Nous rencontrer</a></li>';
+        $items .= '<li class="menu-item"><a href="' . $adminlink . '">Admin</a></li>';
+        $items .= '<li class="menu-commander"><a href="' . site_url('/commander') . '">Commander</a></li>';
+        return $items;
+
+    } else {
+        $items .= '<li class="menu-item"><a href="' . site_url('/nous-rencontrer') . '">Nous rencontrer</a></li>';
+        $items .= '<li class="menu-commander"><a href="' . site_url('/commander') . '">Commander</a></li>';
+        return $items;
+    }
+}
+
+
 	
 	
 	
